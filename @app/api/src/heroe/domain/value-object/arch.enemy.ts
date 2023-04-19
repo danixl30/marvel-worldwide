84660420146107cbce5cca043f80e9389384b2b4
Daniel Fuentes <titofuentes1000@gmail.com>
2023-04-19 18:18:07 -0400

    feat(Root): domain
    Improving enemies and arch enemies

--- @app/api/src/heroe/domain/value-object/arch.enemy.ts
+++ @app/api/src/heroe/domain/value-object/arch.enemy.ts
@@ -2,8 +2,20 @@ import { ValueObject } from 'src/core/domain/value-objects/value.object'
 import { regExpUUID } from 'src/utils/reg-exps/UUID'
 
 export class ArchEnemy implements ValueObject<ArchEnemy> {
-    private constructor(private readonly id: string) {
-        if (!regExpUUID.test(id)) throw new Error('Invalid arch enemy id')
+    private constructor(
+        private readonly _id: string,
+        private readonly _name: string,
+    ) {
+        if (!regExpUUID.test(this.id) || !this.name)
+            throw new Error('Invalid arch enemy id')
+    }
+
+    get id() {
+        return this._id
+    }
+
+    get name() {
+        return this._name
     }
 
     public get value(): string {
